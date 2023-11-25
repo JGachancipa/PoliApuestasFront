eda2e6c8c68e8cbd8b8c7f02136dc1a620bf9de0
Juan Sebastian Gachancipa Avila <jgachancipa@poligran.edu.co>
2023-11-25 17:03:14 -0500

    Cambio direccion Edit

--- src/Util/genericFunctions/OptionsColumn.js
+++ src/Util/genericFunctions/OptionsColumn.js
@@ -11,7 +11,7 @@ export const OptionsColumn = (getValue, handleEdit, deleteAPISInfo, url, section
     return (
         <div style={{ fontSize: '20px' }}>
             <i className="bi bi-pen-fill"
-                onClick={() => handleEdit(getValue)}>
+                onClick={() => handleEdit(getValue, section)}>
             </i>
             <i className="bi bi-x-circle-fill m-2"
                 onClick={() => { deleteAPISInfo && deleteAPISInfo(getValue, url, section) }}>
